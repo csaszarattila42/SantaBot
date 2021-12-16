08f1afe2c571d6e58b9d01b985f7af046196b16c
csaszarattila42 <csaszarattila90@gmail.com>
2021-12-16 09:15:40 +0100

    Fix tests

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import karma_routine
 
 
 class KarmaRoutineTest(unittest.TestCase):
-    def load_action_types_skips_field_names(self):
+    def test_load_action_types_skips_field_names(self):
         actions_dict = karma_routine.load_action_types("karma.csv")
         with self.assertRaises(KeyError):
             print(actions_dict["action"])
 
-    def load_action_types_actually_loads(self):
-        actions_dict = karma_routine.load_action_types("karma.cvs")
+    def test_load_action_types_actually_loads(self):
+        actions_dict = karma_routine.load_action_types("karma.csv")
         self.assertEqual(actions_dict["gave money to a charity"], "nice")
         self.assertEqual(actions_dict["listened to their partner"], "nice")
         self.assertEqual(actions_dict["lied to a friend"], "naughty")

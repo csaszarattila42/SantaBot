4d772e69eaeeedb35a37344db534ca6695228bea
csaszarattila42 <csaszarattila90@gmail.com>
2021-12-16 20:19:19 +0100

    Add load_person_profiles function

--- karma_routine.py
+++ karma_routine.py
@@ -8,3 +8,14 @@ def load_action_types(filename):
         next(action_csv_reader)
         result = {action[1]: action[0] for action in action_csv_reader}
     return result
+
+
+def load_person_profiles(filename):
+    result = []
+    with open(filename, newline="") as profile_file:
+        profile_reader = csv.DictReader(profile_file, delimiter=";")
+        for person in profile_reader:
+            person["actions"] = person["actions"].split(",")
+            person["ideal present categories"] = person["ideal present categories"].split(",")
+            result.append(person)
+    return result
